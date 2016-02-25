f6cc6ad5d261763cdb429f6f7fa3d692bb9be0a3
David Hatch <dhatch387@gmail.com>
2016-02-24 20:02:22 -0500

    LockManagerA: Initial implementation passes included tests.

--- txn/lock_manager.cc
+++ txn/lock_manager.cc
@@ -4,17 +4,34 @@
 // Lock manager implementing deterministic two-phase locking as described in
 // 'The Case for Determinism in Database Systems'.
 
+#include <deque>
+
 #include "txn/lock_manager.h"
+
+using std::deque;
 
 LockManagerA::LockManagerA(deque<Txn*>* ready_txns) {
   ready_txns_ = ready_txns;
 }
 
 bool LockManagerA::WriteLock(Txn* txn, const Key& key) {
-  // CPSC 438/538:
-  //
-  // Implement this method!
-  return true;
+  bool empty = true;
+  LockRequest rq(EXCLUSIVE, txn);
+  deque<LockRequest> *dq = lock_table_[key];
+
+  if (dq == NULL) {
+    dq = new deque<LockRequest>();
+    lock_table_[key] = dq;
+  } else {
+    empty = dq->empty();
+  }
+
+  dq->push_back(rq);
+
+  if (!empty) { // Add to wait list, doesn't own lock.
+    txn_waits_[txn]++;
+  }
+  return empty;
 }
 
 bool LockManagerA::ReadLock(Txn* txn, const Key& key) {
@@ -24,16 +41,40 @@ bool LockManagerA::ReadLock(Txn* txn, const Key& key) {
 }
 
 void LockManagerA::Release(Txn* txn, const Key& key) {
-  // CPSC 438/538:
-  //
-  // Implement this method!
+  deque<LockRequest> *queue = lock_table_[key];
+  bool removedOwner = true; // Is the lock removed the lock owner?
+
+  // Delete the txn's exclusive lock.
+  for (auto it = queue->begin(); it < queue->end(); it++) {
+    if (it->txn_ == txn) { // TODO is it ok to just compare by address?
+        queue->erase(it);
+        break;
+    }
+    removedOwner = false;
+  }
+
+  if (!queue->empty() && removedOwner) {
+    // Give the next transaction the lock
+    LockRequest next = queue->front();
+
+    int wait_count = --txn_waits_[next.txn_];
+    if (wait_count == 0) {
+        ready_txns_->push_back(next.txn_);
+        txn_waits_.erase(next.txn_);
+    }
+  }
 }
 
 LockMode LockManagerA::Status(const Key& key, vector<Txn*>* owners) {
-  // CPSC 438/538:
-  //
-  // Implement this method!
-  return UNLOCKED;
+  deque<LockRequest> *dq = lock_table_[key];
+  if (dq == NULL || dq->empty()) {
+    return UNLOCKED;
+  } else {
+    vector<Txn*> _owners;
+    _owners.push_back(dq->front().txn_);
+    *owners = _owners;
+    return EXCLUSIVE;
+  }
 }
 
 LockManagerB::LockManagerB(deque<Txn*>* ready_txns) {
